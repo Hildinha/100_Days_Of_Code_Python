8c6c32d70080f377cf6c60dc0b0c292abb60d853
Hilda Lubian <hildalubian125@gmail.com>
2025-01-21 17:27:33 -0300

    Adding the Band Name Generator python script that was made in 100 Days Of Code Couse

---
+++ simple_projects/band_name_generator.py
@@ -0,0 +1,5 @@
+# This is a simple python script that was made in 100 Days Of Code Couse on day 1
+print("Welcome to the Band Name Generator")
+name_of_city = input("What\'s the name of the city you grew up in?\n")
+name_pet = input("What\'s your pet\'s name?\n")
+print(f"Your band name cloud be {name_of_city} {name_pet}")
